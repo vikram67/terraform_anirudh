94c5a7edd930c72af9c310e439e780e90e99dfe0
vikram67 <vikram.sanugula67@gmail.com>
2022-11-03 10:36:35 +0530

    updated changes to elb variables

--- elb_variables.tf
+++ elb_variables.tf
@@ -1,7 +1,33 @@
+##Load balancer details
 variable "owner" {
   description = "The owner details"
   type        = string
   default     = ""
+}
+
+variable "elb_type" {
+  description = "The type of Load balancer to create"
+  type = string
+  default = "application"
+}
+
+variable "env_code" {
+  description = "Target environment name (dev|stg|prd)"
+  type = string
+}
+
+variable "deletion_protection" {
+  description = "It sets to protect elb from accidental deletion"
+  type = bool
+  default = true
+  
+}
+
+variable "ip_address_type" {
+  description = "Type of IP address used for ELB"
+  type = string
+  default = "ipv4"
+  
 }
 
 variable "app_security_group_cfn_stack_name" {
@@ -50,4 +76,92 @@ variable "component_short_name" {
   description = "The component short namew` 32Q2`   ` details"
   type        = string
   default     = ""
+}
+
+variable "project_key" {
+  description = "The component short namew` 32Q2`   ` details"
+  type        = string
+  default     = ""
+}
+
+
+##Target group
+
+variable "name" {
+  type        = string
+  default     = ""
+}
+
+variable "port" {
+  type = string
+}
+
+variable "protocol" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string 
+}
+
+variable "stickiness" {
+  type = string
+}
+
+variable "sticky_type" {
+    type = string  
+}
+  
+variable "health_check_path" {
+    type = string
+}
+  
+variable "health_check_port" {
+  type = string
+}
+
+variable "health_check_protocol" {
+  type = string
+}
+
+variable "health_check_timeout" {
+  type = string
+}
+
+variable "health_check_interval" {
+  type = string
+}
+
+variable "health_check_matcher" {
+  type = string
+}
+
+variable "health_check_healthy_threshold" {
+  type = string
+}
+
+variable "health_check_unhealthy_threshold" {
+  type = string
+}
+
+##Listener
+
+variable "load_balancer_arn" {
+  type = string
+}
+
+variable "port" {
+  type = number
+}
+
+variable "certificate_arn" {
+   type = string
+}
+
+variable "target_group_arn" {
+  type = string
+}
+
+variable "target_group_type" {
+  type = string
 }
