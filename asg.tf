2a139e6fd787c568f4bce35243ffe1cd0f95e57a
vikram67 <vikram.sanugula67@gmail.com>
2022-11-03 15:04:14 +0530

    updated asg file

---
+++ asg.tf
@@ -0,0 +1,24 @@
+module "ASG" {
+  source  = ""
+
+  # Autoscaling group
+
+  min_size                  = var.asg_min_size
+  max_size                  = var.asg_max_size
+  desired_capacity          = var.desired_capacity
+  pattern_name              = var.pattern_name
+  ec2_instance_type         = var.ec2_instance_type
+  health_check_type         = "EC2"
+  owner                     = var.owner
+  owner_contact             = var.owner_contact
+  managed_by                = var.managed_by
+  tier                      = var.tier
+  ami_os                    = var.ami_os
+  ami_os_version            =  var.ami_os_version
+  ami_identifier            = var.ami_identifier
+  ami_namespace             = var.ami_namespace
+  is_behind_lb              = var.is_behind_lb
+  ec2_iam_instance_profile  = var.ec2_iam_instance_profile
+
+
+}
