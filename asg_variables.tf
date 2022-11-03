e8d4293a7d9c82d358225515e663e11a875171d3
vikram67 <vikram.sanugula67@gmail.com>
2022-11-03 15:13:39 +0530

    asg

---
+++ asg_variables.tf
@@ -0,0 +1,71 @@
+variable "asg_min_size" {
+  description = "The minimum size of the autoscaling group"
+  type        = number
+  default     = 1
+}
+
+variable "asg_max_size" {
+  description = "The maximum size of the autoscaling group"
+  type        = number
+  default     = 4
+}
+
+variable "desired_capacity" {
+  description = "The number of Amazon EC2 instances that should be running in the autoscaling group"
+  type        = number
+  default     = 2
+}
+
+variable "ec2_instance_type" {
+  description = "The instance type"
+  type        = string
+  default     = ""
+}
+
+variable "ec2_iam_instance_profile" {
+  description = "The instance role"
+  type        = string
+  default     = ""
+}
+
+variable "is_behind_lb" {
+  description = "The asg would be behind the load balancer"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_id" {
+  description = "The vpc details the asg will be created"
+  type        = string
+  default     = ""
+}
+
+variable "os_family" {
+  description = "Operating family details"
+  type        = string
+  default     = ""
+}
+
+variable "ami_os" {
+  description = "The ami os details"
+  type        = string
+  default     = ""
+}
+
+variable "ami_identifier" {
+  description = "The ami identifier details"
+  type        = string
+  default     = ""
+}
+
+variable "application_name" {
+  description = "The application name details"
+  type        = string
+  default     = ""
+}
+
+variable "owner" {
+  description = "The owner details"
+  type        = string
+  default     = ""
+}
